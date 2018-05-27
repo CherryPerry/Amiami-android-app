aaf8e2394f3a771089989be10ffec03434ebdb96
CherryPerry <ya.cherryperry@yandex.ru>
2018-05-27 17:59:44 +0300

    Use OkHttp cache, 304 Not Modified was implemented on server

--- app/src/main/kotlin/ru/cherryperry/amiami/network/APIModule.kt
+++ app/src/main/kotlin/ru/cherryperry/amiami/network/APIModule.kt
@@ -1,7 +1,9 @@
 package ru.cherryperry.amiami.network
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -10,22 +12,27 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import ru.cherryperry.amiami.BuildConfig
 import rx.schedulers.Schedulers
+import java.io.File
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 class APIModule {
+
     @Provides
     @Singleton
-    fun okHttpClient(): OkHttpClient {
+    fun okHttpClient(context: Context): OkHttpClient {
         val logging = HttpLoggingInterceptor()
         logging.level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.HEADERS else HttpLoggingInterceptor.Level.NONE
-
+        val cacheDir = File(context.cacheDir, "http")
+        cacheDir.mkdirs()
+        val cache = Cache(cacheDir, 5 * 1024 * 1024)
         return OkHttpClient.Builder()
                 .addNetworkInterceptor(logging)
-                .readTimeout(1, TimeUnit.MINUTES)
-                .writeTimeout(1, TimeUnit.MINUTES)
-                .connectTimeout(1, TimeUnit.MINUTES)
+                .cache(cache)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
                 .build()
     }
 
