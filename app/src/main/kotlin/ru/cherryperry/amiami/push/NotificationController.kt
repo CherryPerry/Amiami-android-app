efef6a88f547a27dcb6851f7b525665367e93009
CherryPerry <ya.cherryperry@yandex.ru>
2018-06-10 18:19:03 +0300

    Notification channel support

--- app/src/main/kotlin/ru/cherryperry/amiami/push/NotificationController.kt
+++ app/src/main/kotlin/ru/cherryperry/amiami/push/NotificationController.kt
@@ -1,10 +1,13 @@
 package ru.cherryperry.amiami.push
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.support.v4.app.NotificationCompat
 import ru.cherryperry.amiami.AppPrefs
 import ru.cherryperry.amiami.R
@@ -13,9 +16,13 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class NotificationController @Inject constructor(context: Context, private val prefs: AppPrefs) {
+class NotificationController @Inject constructor(
+        private val context: Context,
+        private val prefs: AppPrefs
+) {
     companion object {
-        private val notificationId = 0
+        private const val notificationId = 0
+        private const val notificationChannelId = "channel.updates"
     }
 
     private val manager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
@@ -32,8 +39,10 @@ class NotificationController @Inject constructor(context: Context, private val p
             val notifyIntent = Intent(context, MainActivity::class.java)
             notifyIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             val pendingIntent = PendingIntent.getActivity(context, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-            val builder = NotificationCompat.Builder(context)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createOrUpdateNotificationChannel()
+            }
+            val builder = NotificationCompat.Builder(context, notificationChannelId)
                     .setSmallIcon(R.mipmap.ic_notification_icon)
                     .setContentTitle(context.getString(R.string.app_name))
                     .setContentText(context.getString(R.string.notification_counter, value))
@@ -42,5 +51,13 @@ class NotificationController @Inject constructor(context: Context, private val p
 
             manager.notify(notificationId, builder.build())
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createOrUpdateNotificationChannel() {
+        val channel = NotificationChannel(notificationChannelId,
+                context.getString(R.string.notification_channel_title),
+                NotificationManager.IMPORTANCE_LOW)
+        manager.createNotificationChannel(channel)
     }
 }
