6f4503acb8d47ecfc0e4607d8b1d9ede20c36aee
CherryPerry <ya.cherryperry@yandex.ru>
2018-06-11 18:24:35 +0300

    NotificationController refactoring

--- app/src/main/kotlin/ru/cherryperry/amiami/push/NotificationController.kt
+++ app/src/main/kotlin/ru/cherryperry/amiami/push/NotificationController.kt
@@ -7,7 +7,6 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Build
-import android.support.annotation.RequiresApi
 import android.support.v4.app.NotificationCompat
 import ru.cherryperry.amiami.AppPrefs
 import ru.cherryperry.amiami.R
@@ -25,39 +24,53 @@ class NotificationController @Inject constructor(
         private const val notificationChannelId = "channel.updates"
     }
 
-    private val manager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    private val notificationManager: NotificationManager? =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    init {
+        createOrUpdateNotificationChannel()
+    }
 
     fun reset() {
-        manager.cancel(notificationId)
+        notificationManager?.cancel(notificationId)
         prefs.pushCounter = 0
     }
 
     fun show(value: Int, context: Context) {
-        prefs.pushCounter += prefs.pushCounter
-
-        if (prefs.push) {
+        notificationManager?.apply {
+            if (!prefs.push) {
+                return
+            }
+            prefs.pushCounter += prefs.pushCounter
             val notifyIntent = Intent(context, MainActivity::class.java)
             notifyIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             val pendingIntent = PendingIntent.getActivity(context, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                createOrUpdateNotificationChannel()
+            if (hasNotificationChannel()) {
+                val builder = NotificationCompat.Builder(context, notificationChannelId)
+                        .setSmallIcon(R.mipmap.ic_notification_icon)
+                        .setContentTitle(context.getString(R.string.app_name))
+                        .setContentText(context.getString(R.string.notification_counter, value))
+                        .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.mipmap.ic_launcher))
+                        .setContentIntent(pendingIntent).setAutoCancel(true)
+                notify(notificationId, builder.build())
             }
-            val builder = NotificationCompat.Builder(context, notificationChannelId)
-                    .setSmallIcon(R.mipmap.ic_notification_icon)
-                    .setContentTitle(context.getString(R.string.app_name))
-                    .setContentText(context.getString(R.string.notification_counter, value))
-                    .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.mipmap.ic_launcher))
-                    .setContentIntent(pendingIntent).setAutoCancel(true)
-
-            manager.notify(notificationId, builder.build())
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     private fun createOrUpdateNotificationChannel() {
-        val channel = NotificationChannel(notificationChannelId,
-                context.getString(R.string.notification_channel_title),
-                NotificationManager.IMPORTANCE_LOW)
-        manager.createNotificationChannel(channel)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return
+        }
+        notificationManager?.apply {
+            val channel = NotificationChannel(notificationChannelId,
+                    context.getString(R.string.notification_channel_title),
+                    NotificationManager.IMPORTANCE_LOW)
+            createNotificationChannel(channel)
+        }
+    }
+
+    private fun hasNotificationChannel(): Boolean {
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.O
+                || notificationManager?.getNotificationChannel(notificationChannelId) != null
     }
 }
