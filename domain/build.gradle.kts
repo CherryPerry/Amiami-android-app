4fa6c9167fc6b7409e6ade01c33e7c08b3004fb6
CherryPerry <ya.cherryperry@yandex.ru>
2019-07-27 23:03:31 +0100

    Use Gradle 5.5.1

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -25,10 +25,10 @@ android {
 dependencies {
     api(project(":core"))
 
-    api("org.jetbrains.kotlin:kotlin-stdlib:${ext["kotlinVersion"]}")
+    api("org.jetbrains.kotlin:kotlin-stdlib:${property("kotlinVersion")}")
 
-    kapt("com.google.dagger:dagger-compiler:${ext["daggerVersion"]}")
+    kapt("com.google.dagger:dagger-compiler:${property("daggerVersion")}")
 
-    testImplementation("junit:junit:${ext["junitVersion"]}")
-    testImplementation("org.mockito:mockito-core:${ext["mockitoVersion"]}")
+    testImplementation("junit:junit:${property("junitVersion")}")
+    testImplementation("org.mockito:mockito-core:${property("mockitoVersion")}")
 }
